59b32bfda6f808043beb4d089f537cb6676049eb
Vaibhav <vaibhav7895@gmail.com>
2023-03-30 23:29:45 +0530

    day2 again

--- my-app/src/pages/SignUp.jsx
+++ my-app/src/pages/SignUp.jsx
@@ -87,7 +87,6 @@ export default function Signup() {
               userlastNAme: lastName,
               
             })
-            
           )
           setEmail("");
           setPassword("");
